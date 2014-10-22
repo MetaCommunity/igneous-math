a30ef0d2f846dd4bc82b478ab582d702b976b05c
Sean Champ <spchamp@users.sourceforge.net>
2014-10-22 15:20:08 -0700

    cleanups; stubs for project documentation

--- src/main/cltl/igneous-math.lisp
+++ src/main/cltl/igneous-math.lisp
@@ -265,7 +265,7 @@
   ()
   (:report
    (lambda (c s)
-     (format s "No measureent class registered for name ~S"
+     (format s "No measurement class registered for name ~S"
              (entity-not-found-name c)))))
 
    
@@ -520,6 +520,7 @@ See also: `rescale'"))
 
 (deftype prefix-degree ()
   '(and (integer -24 24)
+    ;; formally onto the SI standard for decimal prefixes
     (member 0 
      1 2 3 6 9 12 15 18 21 24
      -1 -2 -3 -6 -9 -12 -15 -18 -21 -24)))
@@ -577,14 +578,14 @@ See also: `rescale'"))
   ()
   (:report
    (lambda (c s)
-     (format s "No measureent prefix registered for name ~S"
+     (format s "No measurement prefix registered for name ~S"
              (entity-not-found-name c)))))
 
 (define-condition prefix-degree-not-found (entity-not-found)
   ()
   (:report
    (lambda (c s)
-     (format s "No measureent prefix registered for degree ~S"
+     (format s "No measurement prefix registered for degree ~S"
              (entity-not-found-name c)))))
 
 
@@ -774,7 +775,6 @@ See also: `rescale'"))
   (rescale scalar (prefix-degree prefix)))
 
 
-
 (defmethod nrescale ((scalar measurement) (prefix fixnum))
   (multiple-value-bind (new-mag new-deg) 
       (shift-magnitude (measurement-magnitude scalar)
@@ -870,22 +870,66 @@ See also: `rescale'"))
 
 ;;; % Geometry Domain
 
+;; TO DO
 
-(defclass scalar (measurement) ;; a monadic coordinate
+(defclass scalar (measurement) 
+  ;; effectively a monadic coordinate
   ())
 
 
+;;; % Concepts of Plane and Space
+
+
 ;; (defclass plane ...)
-;; (defclass space ...)
+
+;; (defclass euclidian-plane ...)
+;; ^ alternate name for cartesian coordinate plane ?(?)
 ;;
+;; (defclass rectangular-plane ...)
+;; ^ a coordiate "two space" onto (i, j), complex number system
+
+;; (defclass space ...)
+;; ... reference vectors ...
+
+;;; % Concepts of Point and Location
+
 ;; The term "Coordinate" will be applied informaly, in the following
 ;;
-;; (defclass diadic-coordinate ...) ;; a <point> onto a <planar surface>
-;; (defclass triadic-coordinate ...) ;; a <point> onto <3 space>
+;; (defclass diadic-coordinate ...) 
+;; ^ a <point> onto a <planar surface>
+;;
+;; (defclass triadic-coordinate ...) 
+;; ^ a <point> onto orthogonal <3 space>
+;;
 ;; (defclass polar-coordinate ...) 
 ;; ^ a <point> within a <polar coordinate> system  on a <planar surface>
+;;
 ;; (defclass spherical-coordinate ...)
-;; ^ a <point> within a <spherical coordinate space>
+;; ^ a <point> within a <spherical coordinate space>,
+;;   using a single reference model for spherical coordinates
+;;   cf. RA, AZ, DEC, and broader astrometry
 
+;;; % VECTOR as a formal mathematical object type
 
+;; Note that this sytem will use radian notation for vectors, internally
 
+;; (def-frob* *null-vector* ...)
+
+;; (defun null-vector-p vector ...)
+
+;; (def-frob @= vector vector)
+;; (def-frob @+ vector vector)
+;; (def-frob @* scalar vector) ;; vector dot product
+;; (def-frob @* vector vector) ;; vector cross product
+
+;; (def-frob @=@ &rest objects) ;; tail recursion
+
+;; thence to extend this system onto the integral and differential
+;; calculus, with applications for frequency domain analysis of 
+;; electronic system components
+
+;; to do: add a seperate CLIM presentations system, focusing on
+;; applications within electronic design and analysis,
+;; and emphasiziing CLIM's definition of a standard approach for
+;; visualization of arbitrary coordinate systems onto a planar
+;; display screen
