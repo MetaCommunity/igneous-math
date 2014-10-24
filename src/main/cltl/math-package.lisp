72171e0d9aa8d864d2c0662d6a219c877ff6e515
Sean Champ <spchamp@users.sourceforge.net>
2014-10-24 08:40:09 -0700

    port for CMUCL, CCL

--- src/main/cltl/math-package.lisp
+++ src/main/cltl/math-package.lisp
@@ -9,11 +9,24 @@
    #:cl
    #:defgeneric
    #:defmethod
-   #:standard-generic-function)
-  
+   #:standard-generic-function
+   #:standard-method
+   #:standard-class)
+  #+(or SBCL CMU CCL)
+  (:shadowing-import-from
+   #+SBCL #:SB-MOP
+   #+CMU #:PCL
+   #+CCL #:CCL
+   #:validate-superclass
+   )
+
   (:use #:info.metacommunity.cltl.utils
         #:bordeaux-threads
         #:c2mop
         #:cl))
 
 
+(in-package #:math)
+;; FIXME: "Do this somewhere else"
+#-(or SBCL CMU)
+(declaim (declaration values))
