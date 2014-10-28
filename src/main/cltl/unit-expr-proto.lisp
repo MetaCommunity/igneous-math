ac414cabd2291bcbdb0912c8376ef9d1abb73178
Sean Champ <gimbal+github@metacommunity.info>
2014-10-28 16:58:48 -0700

    implement initial measurement+ prototype

--- src/main/cltl/unit-expr-proto.lisp
+++ src/main/cltl/unit-expr-proto.lisp
@@ -42,3 +42,80 @@
 	(u2 (make-expr :m)))
     (values (unit* u1 u2)
 	    #+TO-DO (unit+ u1 u2))))
+
+;; ---------
+
+#+PROTOTYPE-2
+(defun compute-measurement (op u1 u2)
+  ;; frob - prototype
+  (ecase op
+    ((+ -) (if (eq u1 u2)
+	       (values u1)
+	       (query-measurement op u1 u2)))
+    (* (if (eq u1 u2)
+	   (query-measurement-squared u1)
+	   (query-measurement op u1 u2)))
+    (/ (if (eq u1 u2)
+	   (find-the-unit-measurement u1)
+	   (query-measurement op u1 u2)))))
+
+#+PROTOTYPE-2
+(defun domain-analyze (op m1 m2)
+  ;; values: base-unit, u1-factor, u1-factor-exponent,
+  ;;         u2-factor, u2-factor-exponent
+  (let* ((unit-1 (class-of m1))
+	 (unit-2 (class-of m2))
+	 (domain-1 (class-of unit-1))
+	 (domain-2 (class-of unit-2))
+	 (base-1 (measurement-domain-base-measure domain-1))
+	 (base-2 (measurement-domain-base-measure domain-2)))
+    (let ((unit (compute-measurement op base-1 base-2)))
+      (values unit 
+	      (measurement-base-factor unit-1)
+	      (measurement-base-factor-exponent unit-1)
+	      (measurement-base-factor unit-2)
+	      (measurement-base-factor-exponent unit-2)))))
+
+#+PROTOTYPE-2
+(labels ((m* (m1 m2)
+	   (multiple-value-bind (base-unit m1-factor m1-factor-exponent
+					   m2-factor m2-factor-exponent)
+	       (domain-analyze '* m1 m2)
+	     (make-expr base-unit
+			:factor
+			(* (expr-factor m1) m1-factor
+			   (expr-factor m2) m2-factor))))
+	 (m+ (m1 m2)
+	   (multiple-value-bind (base-unit m1-factor m1-factor-exponent
+					   m2-factor m2-factor-exponent)
+	       (domain-analyze '+ m1 m2)
+	       (let ((m1-m (measurement-magnitude m1))
+		     (m1-d (measurement-degree m1))
+		     (m2-m (measurement-magnitude m2))
+		     (m2-d (measurement-degree m2)))
+		 (make-measurement  
+		  ;; FROB not handling DEGREE per decimal scaling,
+		  ;; not seperate to handling the measurement magnitudes
+		  (+ (* m1-m m1-factor 
+			(expt 10 m1-factor-exponent) 
+			(expt 10 m1-d))
+		     (* m2-m m2-factor 
+			(expt 10 m2-factor-exponent) 
+			(expt 10 m2-d)))
+		  base-unit)))))
+  (let ((m1 (make-measurement 1 :|m|))
+	(m2 (make-measurement 1 :|ft|)))
+
+    (let ((plus-test (m+ m1 m2)))
+    (values #+TO-DO (m* m1 m2)
+
+	    plus-test 
+	    (float (base-magnitude plus-test ) pi)
+	    (= (base-magnitude plus-test)
+	       (+ 1 (/ 1200 3937)))
+	    )
+    
+    ;; "CHECK" on the PLUS-TEST
+    )))
+
+
