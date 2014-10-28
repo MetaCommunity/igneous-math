9c6309ae990d77215e9d89e5e093851a60994706
Sean Champ <gimbal+github@metacommunity.info>
2014-10-28 11:02:43 -0700

    Define %DOMAINS%, %DOMAINS-LOCK% as top-level forms, with documentation strings

--- src/main/cltl/domain.lisp
+++ src/main/cltl/domain.lisp
@@ -42,44 +42,53 @@
 	     sym n p)))))
 
 
-(let ((%domains% (make-array 7 :fill-pointer 0))
-      (%domains-lock% (make-lock "%DOMAINS%")))
+(declaim (type (vector measurement-domain) %domains% ))
 
-  (defun find-measurement-domain (name)
-    ;; FIXME: #I18N query
-    (declare (type (or symbol string) name)
-	     (values measurement-domain))
-    (let ((s (etypecase name
-	       (symbol name)
-	       (string (intern* (read-from-string name)
-				(find-package '#:keyword))))))
-      (with-lock-held (%domains-lock%)
-	(or (find s %domains%
-		  :test #'eq
-		  :key #'measurement-domain-symbol)
-	    (error 'measurement-domain-not-found
-		   :name name)))))
-  
-  (defun register-measurement-domain (domain)
-    (declare (type measurement-domain domain)
-	     (values measurement-domain))
+(defvar %domains% (make-array 7 :fill-pointer 0 
+			      :element-type 'measurement-domain)
+  "Internal storage for measurement domains.
+
+This variable should be accessed with `%DOMAINS-LOCK%' held")
+
+
+(defvar %domains-lock% (make-lock "%DOMAINS%")
+  "Mutex lock for accessing `%DOMAINS%'")
+
+
+
+(defun find-measurement-domain (name)
+  ;; FIXME: #I18N query
+  (declare (type (or symbol string) name)
+	   (values measurement-domain))
+  (let ((s (etypecase name
+	     (symbol name)
+	     (string (intern* (read-from-string name)
+			      (find-package '#:keyword))))))
     (with-lock-held (%domains-lock%)
-      (let* ((s (measurement-domain-symbol domain))
-	     (n (position s %domains%
-		      :test #'eq
-		      :key #'measurement-domain-symbol)))
-	(cond
-	  (n (warn 'measurement-domain-redefinition
-		   :previous (aref %domains% n)
-		   :new domain)
-	     (setf (aref %domains% n) domain))
-	  (t (vector-push-extend domain %domains%)))
-	(values domain))))
+      (or (find s %domains%
+		:test #'eq
+		:key #'measurement-domain-symbol)
+	  (error 'measurement-domain-not-found
+		 :name name)))))
 
-  (defun enumerate-measurement-domains ()
-    (coerce %domains% 'list))
-  );; LET
+(defun register-measurement-domain (domain)
+  (declare (type measurement-domain domain)
+	   (values measurement-domain))
+  (with-lock-held (%domains-lock%)
+    (let* ((s (measurement-domain-symbol domain))
+	   (n (position s %domains%
+			:test #'eq
+			:key #'measurement-domain-symbol)))
+      (cond
+	(n (warn 'measurement-domain-redefinition
+		 :previous (aref %domains% n)
+		 :new domain)
+	   (setf (aref %domains% n) domain))
+	(t (vector-push-extend domain %domains%)))
+      (values domain))))
 
+(defun enumerate-measurement-domains ()
+  (coerce %domains% 'list))
 
 ;; (find-measurement-domain :electrical-current)
 
