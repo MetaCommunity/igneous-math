68b2e8ee081d4a78c0690dff867b8b83cb24e3e6
Sean Champ <gimbal+github@metacommunity.info>
2014-11-17 17:44:26 -0800

    add initial prototype

---
+++ src/main/cltl/matrix-clim.lisp
@@ -0,0 +1,22 @@
+
+(in-package #:clim-user)
+
+;; CLIM-CLX is not loading.
+;; Something about presentation types + defmethod
+;; Refer to debug output in SLIME.
+
+(define-application-frame frob-frame  ()
+  ()
+  (:panes
+   (matrix (let ((m #((1 2 3 4) (4 3 2 1))))
+             (formatting-table 
+              ()
+              (dotimes (i (array-dimension m 0))
+                (formatting-row 
+                 ()
+                 (dotimes (j (array-dimension m 1))
+                   (formatting-cell
+                    (princ (the number (aref m i j))))))))))))
+
+
+            
