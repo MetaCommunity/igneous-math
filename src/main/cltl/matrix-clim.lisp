f1ebab0bf58f419f27dd1e286a13e73135162b6e
Sean Champ <gimbal+github@metacommunity.info>
2014-12-16 03:34:19 -0800

    FROB-FRAME: use DISPLAY-FROB; DISPLAY-FROB: New

--- src/main/cltl/matrix-clim.lisp
+++ src/main/cltl/matrix-clim.lisp
@@ -5,16 +5,35 @@
 ;; Something about presentation types + defmethod
 ;; Refer to debug output in SLIME.
 
+(defun display-frob (frame pane)
+  ;; FIXME: provide the matrix value via an attribute of the PANE
+  ;; FIXME: also draw the conventional brackets on the pane
+  (declare (ignore frame))
+  (let ((*standard-output* pane))
+    (let ((m #2A((1 2 3 4) (4 3 2 1) (4 2 3 1) (1 3 2 4))))
+      (formatting-table ()
+        (dotimes (i (array-dimension m 0))
+          (formatting-row ()
+            (dotimes (j (array-dimension m 1))
+              (formatting-cell ()
+                (present  (aref m i j))
+                ))))))))
+
+
 (define-application-frame frob-frame  ()
   ()
   (:panes
-   (matrix (let ((m #((1 2 3 4) (4 3 2 1))))
-             (formatting-table ()
-               (dotimes (i (array-dimension m 0))
-                 (formatting-row ()
-                   (dotimes (j (array-dimension m 1))
-                     (formatting-cell ()
-                       (with-output-as-presentation (t)
-                         (princ (the number (aref m i j))
-                                t)))))))))))
+   (matrix :application
+           :height 90 :width 100
+           :display-function #'display-frob))
+  (:layouts
+   (default (vertically () matrix))))
+
+#|
+
+(let ((f (make-application-frame 'frob-frame
+                                 :pretty-name "Frob")))
+  (run-frame-top-level f))
+
+|#
 
