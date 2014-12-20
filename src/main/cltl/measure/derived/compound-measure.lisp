5859d72f628ea4c18f8fa4845872f42ddff6e39c
Sean Champ <gimbal+github@metacommunity.info>
2014-12-20 13:01:09 -0800

    define NORMALIZE-UNIT-EXPRESSION; rename accessors

--- src/main/cltl/measure/derived/compound-measure.lisp
+++ src/main/cltl/measure/derived/compound-measure.lisp
@@ -2,35 +2,93 @@
 
 (in-package #:math)
 
-
-(defconstant* +si-base-measurements+
+#+NIL
+(defconstant* +si-base-measurement-metaclasses+ ;; (?)
+    ;; This constant value is defined here, in order to ensure that
+    ;; all of the base measurement unit classes are defined at the
+    ;; time when this form is evaluated
     (make-array 7
                 :initial-contents
                 (mapcar #'find-class
-                        '(length mass time electrical-current
-                          temperature amount-substance
-                          luminous-intensity))))
+                        '(base-length 
+                          base-mass 
+                          base-time 
+                          base-electrical-current
+                          base-temperature 
+                          base-amount-substance
+                          base-luminous-intensity))))
+
+(defgeneric measurement-unit-sequence (class))
+
+(defgeneric measurement-degree-sequence (class))
+
+(defgeneric measurement-normalized-expression (class))
 
 
-(defgeneric measurement-class-unit-sequence (class))
+;; % Compound Measurements - Registry and Retrieval
 
-(defgeneric measurement-class-degree-sequence (class))
+#| Notes
 
-(defgeneric measurement-class-normalized-expression (class))
+* This system will define a registry specifically for compound
+  measurement unit classes, effectively augmenting the registry of
+  base and derived measurement unit classes  comprised of
+  `%MEASUREMENT-CLASSES%' and its corresponding
+  `%MEASUREMENT-CLASSES-LOCK%'
+
+* At time of registering a componound measurement class, the class may
+  be indexed within the compound measurement unit classes registry,
+  according to three primary values:
+
+    0. The identity of the class, itself. Assuming that the Common
+       Lisp implementation ensures that any class Cn named N (n > 1)
+       will always be EQ to class C1 named N, the identity of the
+       class Cn may be applied as an index key, such that the
+       functions storing and accessing the index may use EQ
+       as a :test function.
+
+    1. The MEASUREMENT-NORMALIZED-EXPRESSION defined to the
+       measurement unit class. Although this may seem to denote a 
+       cosmetically appealing method for functional query onto the
+       compound measurement unit classes registry, however it may not
+       present the most optimal approach possible.
+
+    2. The set of the unit sequence and degree sequence for the
+       normalized measurement unit expression defined to
+       the measurement unit class. It may serve to permit for an
+       optimized implementation in the functions for measurement query
+       and unit conversion, when these values would be accessed
+       seperately.
+
+* When searching for a measurement unit class, provided an arbitrary
+  "normalized" base measurement unit expression, it may be assumed:
+
+    * that the elements of the expresion may occur in any arbitrary 
+      order. 
+    * that a subset of the elements of the expression may effectively
+      "match" the complete set of elements of a registered, compound
+      measurement unit class
+    * that the expression may contain elements in addition to any
+      subset of elements effectively "matching" the normalized unit
+      expression of any registered, compound measurement unit class
+
+|#
+
+;; % Compound Measurements - Measurement Class Definition and Initialization
+
 
 (defclass compound-measurement-class (measurement-class)
   ((unit-sequence
     :initarg :unit-sequence
     :type (simple-array measurement-class (*))
-    :reader measurement-class-unit-sequence)
+    :reader measurement-unit-sequence)
    (degree-sequence
     :initarg :degree-sequence
     :type (simple-array fixnum (*))
-    :reader measurement-class-degree-sequence)
+    :reader measurement-degree-sequence)
    (normalized-expression
     :type (simple-array t (* 2))
     :initarg :normalized-expression
-    :reader measurement-class-normalized-expression
+    :reader measurement-normalized-expression
     )))
   
 
@@ -57,7 +115,7 @@
                         (reslt (make-array (list n 2)
                                            :element-type t)))
                    (dotimes (index n reslt)
-                     (let* ((elt (pop expr))
+                     (let* ((elt (aref %expr index))
                             (s (etypecase elt
                                  (symbol elt)
                                  (cons (car elt))))
@@ -102,6 +160,10 @@
                 (getf initargs :degree-sequence) (map-degrees nex))))
       (cond 
         (args-changed-p (apply #'call-next-method instance slots initargs))
+        ;; FIXME: Thie following CALL-NEXT-METHOD form should probably
+        ;; not be denoted as 'unreachable code' by the compiler.
+        ;; In SBCL 1.2.5.76-65a44db, it is denoted as unreachable
+        ;; however. 
         (t (call-next-method))))))
 
 ;; To Do: Develop prototypes for SIMPLIFY-UNIT / NORMALIZE-UNIT
@@ -119,6 +181,9 @@
   (:metaclass measurement-domain)
   (:base-measure . newton))
 
+#+PROTOTYPE
+(register-measurement-domain 'force)
+
 
 #+PROTOTYPE
 (defclass newton (measurement)
@@ -128,24 +193,87 @@
   (:symbol . :|N|)
   (:metaclass compound-measurement-class)
   (:expression :|m| :|kg| (:|s| -2))
-  (:documentation "Measurement class for quanities \
+  (:documentation "Measurement class for quantities \
 of force in base unit newton (N)
 
 Symbolic representation: :N"))
 
-;; (measurement-class-unit-sequence (find-class 'newton))
-;; => #(#<LENGTH METER> #<MASS KILOGRAM> #<TIME SECOND>) [2 times]
+#+NIL ;; instance test
+(measurement-domain-base-measure (find-class 'force))
+;; => #<COMPOUND-MEASUREMENT-CLASS NEWTON>
 
-;; (measurement-class-degree-sequence (find-class 'newton))
-;; => #(1 1 2)
+#+PROTOTYPE
+(register-measurement-class 'newton)
 
-;; (measurement-class-normalized-expression (find-class 'newton))
+#+PROTOTYPE
+(resister-compound-measurement-class 'newton)
+
+;; (measurement-unit-sequence (find-class 'newton))
+;; => #(#<LENGTH METER> #<MASS KILOGRAM> #<TIME SECOND>)
+
+;; (measurement-degree-sequence (find-class 'newton))
+;; => #(1 1 -2)
+
+;; (measurement-normalized-expression (find-class 'newton))
 ;; => #2A((#<LENGTH METER> 1) (#<MASS KILOGRAM> 1) (#<TIME SECOND> -2))
 
 (defclass compound-unit-expr (unit-expr)
   ((elements
     :initarg :elements
     :accessor unit-expr-elements
-    :type simple-vector)))
+    :type (simple-array t (* 2)))))
 
 
+;; NORMALIZE-UNIT - Prototype
+
+(defun normalize-unit-expression (&rest exprs)
+  (declare (values (simple-array t (* 2))))
+  (let* ((%exprs (coerce exprs 'simple-vector))
+         (n (length %exprs))
+         (buffer (make-array n :fill-pointer 0)))
+    (declare (type simple-vector %exprs))
+    (dotimes (index n (make-array (list (length buffer) 2)
+                                  :initial-contents buffer))
+      (let* ((elt (aref %exprs index))
+             (s (etypecase elt
+                  (symbol elt)
+                  (cons (car elt))))
+             (c (find-measurement-class s))
+             (d (etypecase elt
+                  (symbol 1)
+                  (cons (cadr elt)))))
+        (declare (type measurement-class c)
+                 (type fixnum d))
+        (flet ((add-elt (c d buffer)
+                 ;; FIXME: CONSY buffer implementation
+                 (vector-push-extend (list (measurement-symbol c) d) 
+                                     buffer)))
+        (etypecase c
+          ((or base-measurement-class 
+               linear-measurement-class
+               geometric-measurement-class)
+           (add-elt c d buffer))
+          (compound-measurement-class
+           (let ((nex (measurement-normalized-expression c)))
+             (dotimes (%index (array-dimension nex 0))
+               (let ((%c (aref nex %index 0))
+                     (%d (* d (aref nex %index 1))))
+                 (add-elt %c %d buffer)))))))))))
+
+;; NORMALIZE-UNIT / SIMPLIFY-UNIT - Usage cases
+
+;; Prototype: Newtonian force / Weight / Acceleration due to gravity (Units)
+;;
+;; (normalize-unit-expression :|N|)
+;; => #2A((:|m| 1) (:|kg| 1) (:|s| -2))
+;;
+;; (normalize-unit-expression :|m| :|kg| '(:|s| -2))
+;; => #2A((:|m| 1) (:|kg| 1) (:|s| -2))
+
+;; (simplify-unit-expression :|m| :|kg| '(:|s| -2))
+
+;; Prototype: Momentum (Units)
+;;
+;; (normalize-unit-expression :|N| :|s|)
+;; (simplify-unit :|m| :|kg| (:|s| -2) :|s|)
+;; (simplify-unit :|kg| :|m| (:|s| -1))
