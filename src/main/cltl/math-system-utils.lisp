6d0f5a6509bd37a85b5fb6f2c7762a20aa0a3057
Sean Champ <gimbal+github@metacommunity.info>
2014-12-20 11:23:08 -0800

    define VSUBSETP

--- src/main/cltl/math-system-utils.lisp
+++ src/main/cltl/math-system-utils.lisp
@@ -24,3 +24,28 @@
   ((container
     :initarg :container
     :reader container-condition-container)))
+
+
+(defmacro vsubsetp (v1 v2 &key (key nil kp)
+                            (test #'eql tp) 
+                            (test-not nil tnp))
+  ;; cf. subsetp
+  (with-gensym (%v1 %v2 %n1 %n2 %elt
+                    %test %test-not)
+    `(let ((,%v1 ,v1)
+           (,%v2 ,v2)
+           ,@(when tp `((,%test ,test)))
+           ,@(when tnp `((,%test-not ,test-not))))
+       (declare (type (simple-array t (*)) ,%v1 ,%v2))
+       (do-vector (,%elt ,%v1 t)
+         (unless (find ,%elt ,%v2 
+                       ,@(when kp `(:key ,key))
+                       ,@(when tp `(:test ,%test))
+                       ,@(when tnp `(:test-not ,%test-not)))
+           (return nil))))))
+
+;; (vsubsetp #(4 2) #(2 3 4) :test #'=)
+;; => T 
+
+;; (vsubsetp #(4 8) #(2 3 4) :test #'=)
+;; => NIL
